1292bd55f7772c0e8365a664fae195d31ec79208
Nora <norakm@stud.ntnu.no>
2023-09-07 13:18:45 +0200

    merge branch navbar #2

--- prosjekt1/src/components/NavBar/NavBar.tsx
+++ prosjekt1/src/components/NavBar/NavBar.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
-export default function NavBar() {
-  return (
-    <nav>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/Browse">Browse</Link>
-      </div>
-    </nav>
-  );
+export default function NavBar(){
+    return(
+        <nav className="NavBar">
+            <div className="LogoNavBar">
+                {/* Legg inn bilde */}
+                <Link to="/" className="HomeLinkLogo">
+                    <h1>
+                        SipFinder
+                    </h1>
+                </Link>
+            </div>
+            <div className="NavBarLinks">
+                <Link to="/" className="HomeLink">
+                    Home
+                </Link>
+                <Link to="/search" className="SearchLink">
+                    Search
+                </Link>
+            </div>
+        </nav>
+    )
 }
