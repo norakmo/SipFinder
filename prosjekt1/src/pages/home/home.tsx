8bab4da5becd11d933f759924289b550751411a9
Aiman <aimann@stud.ntnu.no>
2023-09-12 10:19:50 +0200

    #5 set up home page with carouselcomponent

--- prosjekt1/src/pages/home/home.tsx
+++ prosjekt1/src/pages/home/home.tsx
@@ -1,7 +1,10 @@
+import CarouselComponent from "../../components/CarouselComponent/CarouselComponent";
+import "./home.css";
+
 export default function Home(){
     return(
-        <div>
-            Home page
+        <div className="CarouselContainer">
+            <CarouselComponent />
         </div>
     )
 }
