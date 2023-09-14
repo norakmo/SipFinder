74fa7235f1e7015c8a7a659ea3665a02762abf71
Aiman <aimann@stud.ntnu.no>
2023-09-14 09:28:04 +0200

    #3 fixed merge with main

--- prosjekt1/src/App.tsx
+++ prosjekt1/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/home/home";
-import './App.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Drink from './components/Drink';
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Drink from "./components/Drink";
 
 import Browse from "./pages/browse/Browse";
 import "./App.css";
@@ -11,24 +11,23 @@ import "./App.css";
 function App() {
   const queryClient = new QueryClient();
 
-
   return (
     <>
       <QueryClientProvider client={queryClient}>
-      <Router>
-        <>
-        <div className="NavigationBar">
-          <NavBar />
-        </div>
-        <div className="space"></div>
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/browse"} element={<Browse />} />
-          <Route path={"/drink"} element={<Drink />} />
-        </Routes>
-        </>
-    </Router>
-    </QueryClientProvider>
+        <Router>
+          <>
+            <div className="NavigationBar">
+              <NavBar />
+            </div>
+            <div className="space"></div>
+            <Routes>
+              <Route path={"/"} element={<Home />} />
+              <Route path={"/browse"} element={<Browse />} />
+              <Route path={"/drink"} element={<Drink />} />
+            </Routes>
+          </>
+        </Router>
+      </QueryClientProvider>
     </>
   );
 }
