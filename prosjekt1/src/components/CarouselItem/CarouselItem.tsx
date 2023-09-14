b2b6665121087c3ee3f7d3e573ca2ed15591ebd6
Aiman <aimann@stud.ntnu.no>
2023-09-14 09:20:56 +0200

    #5 started working on carouselitem

---
+++ prosjekt1/src/components/CarouselItem/CarouselItem.tsx
@@ -0,0 +1,12 @@
+import "./CarouselItem.css"
+
+export default function CarouselItem(){
+    return(
+        <div className="carousel-item">
+            <p>Title</p>
+            <img>Image of drink</img>
+            <p>Ingredients</p>
+            <p>Instructions</p>
+        </div>
+    )
+}
