f20dcde02f19e03660fd6f65c2a77fd7c9053e1c
Aiman <aimann@stud.ntnu.no>
2023-09-14 09:24:31 +0200

    #3 small changes on home page

--- prosjekt1/src/pages/home/home.tsx
+++ prosjekt1/src/pages/home/home.tsx
@@ -1,10 +1,10 @@
 import CarouselComponent from "../../components/CarouselComponent/CarouselComponent";
 import "./home.css";
 
-export default function Home(){
-    return(
-        <div className="CarouselContainer">
-            <CarouselComponent />
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="CarouselContainer">
+      <CarouselComponent />
+    </div>
+  );
 }
