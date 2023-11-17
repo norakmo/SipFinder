c34f0ab549be4f219f20ef4b3000cea643abbf1d
Aiman <aimann@stud.ntnu.no>
2023-11-17 12:32:22 +0100

    test: snapshottests for browse and drink page, #22
    co-programmed with @kaiako

--- prosjekt1/src/test/snapshot.test.tsx
+++ prosjekt1/src/test/snapshot.test.tsx
@@ -1,5 +1,7 @@
 import { render } from "@testing-library/react";
 import Home from "../pages/home/home";
+import Browse from "../pages/browse/Browse";
+import Drink from "../pages/drink/Drink";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { expect, it } from "vitest";
 
@@ -12,5 +14,29 @@ it("renders correctly", () => {
     </QueryClientProvider>
   );
   const rh = render(home);
+  expect(rh).toMatchSnapshot();
+});
+
+//Snapshot test for browse page
+it("renders correctly", () => {
+  const queryClient = new QueryClient();
+  const browse = (
+    <QueryClientProvider client={queryClient}>
+      <Browse />
+    </QueryClientProvider>
+  );
+  const rh = render(browse);
+  expect(rh).toMatchSnapshot();
+});
+
+//Snapshot test for drink page
+it("renders correctly", () => {
+  const queryClient = new QueryClient();
+  const drink = (
+    <QueryClientProvider client={queryClient}>
+      <Drink />
+    </QueryClientProvider>
+  );
+  const rh = render(drink);
   expect(rh).toMatchSnapshot();
 });
